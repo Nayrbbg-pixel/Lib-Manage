51d826fce8f70aef0533cedaf1f2197dd95272ea
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2024-12-13 11:41:53 +0530

    Routers separation commit

--- ts.py
+++
@@ -1,8 +0,0 @@
-from database import SessionLocal
-from models import User
-
-
-user = SessionLocal().query(User).filter(User.username=='Aryan Raj').first()
-print(
-    f"{user.username} | {user.role.value} | {user.password}"
-)
