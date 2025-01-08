005f7244a62d08112206fe5f407143b79156a132
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2025-01-08 16:25:44 +0530

    Shifted database to localstorage for better compatability for ease

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/lib_manage"
+# DATABASE_URL = "postgresql://postgres:password@localhost:5432/lib_manage"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL2 = 'sqlite:///./betatesting.db'
+
+engine = create_engine(DATABASE_URL2,
+                       connect_args={
+                           'check_same_thread':False
+                       })
 
 SessionLocal = sessionmaker(bind=engine,
                             autoflush=False,
