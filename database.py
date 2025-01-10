28d5a6fff59e3f3e4ade948aa3f66869466600e4
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2025-01-10 18:24:03 +0530

    Shifted to permanent database

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql://postgres:password@localhost:5432/lib_manage"
+DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a:5432/pacific_library"
 
 DATABASE_URL2 = 'sqlite:///./betatesting.db'
 
-engine = create_engine(DATABASE_URL2,
-                       connect_args={
-                           'check_same_thread':False
-                       })
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine,
                             autoflush=False,
