8125caa1051008700a33e62214e890c8b20b0e75
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2025-01-10 20:43:48 +0530

    Fix for db

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a.singapore-postgres.render.com/pacific_library"
+DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a.singapore-postgres.render.com/pacific_library"
 
 DATABASE_URL2 = 'sqlite:///./betatesting.db'
 
-engine = create_engine(DATABASE_URL2,
-                       connect_args={
-                           'check_same_thread':False
-                       })
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine,
                             autoflush=False,
