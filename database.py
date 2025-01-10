9c2419063ed2a3d054185255b7160c146f4d8a18
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2025-01-10 19:19:01 +0530

    Finished designing Login and register page

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a:5432/pacific_library"
+DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a.singapore-postgres.render.com/pacific_library"
 
 DATABASE_URL2 = 'sqlite:///./betatesting.db'
 
