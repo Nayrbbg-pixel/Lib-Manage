4e9644b0a18f92913f1f91d9ee5c5df90c4caa1b
Nayrbbg-pixel <139642991+Nayrbbg-pixel@users.noreply.github.com>
2025-02-22 21:16:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://pacific_library_user:Fw1u3sIagil9R1JbUNiG9txOc4xR7eIA@dpg-cu0hf15umphs738458h0-a.singapore-postgres.render.com/pacific_library"
+DATABASE_URL = "postgresql://pacific_library_jqt6_user:lA5XZaPTHez0foBtyhd8kKmZ3Y5UELK4@dpg-cusuv32j1k6c738838tg-a/pacific_library_jqt6"
 
 DATABASE_URL2 = 'sqlite:///./betatesting.db'
 
